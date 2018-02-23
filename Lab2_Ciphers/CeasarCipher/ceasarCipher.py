1ece20042d72380e03fa15f5489348e0241b0cc0
Scott Roberts <s.e.roberts0@gmail.com>
2018-02-23 00:21:23 -0800

    Fixing up Dependencies

--- Lab2_Ciphers/CeasarCipher/ceasarCipher.py
+++ Lab2_Ciphers/CeasarCipher/ceasarCipher.py
@@ -7,21 +7,16 @@
 import enchant
 import assignment1Quest as questions
 
-dictionary = enchant.Dict("en_US")
-
 #Assignment Strings
 
-# use dictionary.check("") to see if a word exists
-
-# to get a list from a delimeted string, use str.split("")
-# the parameter of split is the delimeter
-
+# Shift each ASCII character by a given "Shift" amount.
 def ceasarShiftCharacter(char = "", shift = 0):
     shiftedValue = (ord(char) + shift)
     if(shiftedValue >= 123):
         shiftedValue = shiftedValue %123 + 97
     return chr(shiftedValue)
 
+# Apply ceasarShiftCharacter on a string for 26 iterations
 def ceasarShiftList(encString = ""):
     encString = encString.replace(" ", "")
     for s in range(26):
